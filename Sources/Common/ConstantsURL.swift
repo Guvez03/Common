4e447a066a22a7abb50ae7f744ee2f0f3ce5e7f8
Ahmet Güvez <ahmetguvez03@hotmail.com>
2022-06-20 13:29:10 +0300

    added Constants to repo

---
+++ Sources/Common/ConstantsURL.swift
@@ -0,0 +1,15 @@
+//
+//  Exte.swift
+//  MovieDBProject
+//
+//  Created by Ahmet Güvez on 31.05.2022.
+//
+
+import Foundation
+
+public enum Constants{
+    public static let baseImageEndpointUrl = "https://image.tmdb.org/t/p/w500/"
+}
+
+
+
