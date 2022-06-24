fadb68d014098c76060ce628af0f82677c42a2b2
Ahmet Güvez <ahmetguvez03@hotmail.com>
2022-06-25 02:41:34 +0300

    bug fix and registerSupplementaryView extension

--- Sources/Common/Reusable+Extension.swift
+++ Sources/Common/Reusable+Extension.swift
@@ -7,6 +7,11 @@
 //
 import UIKit
 
+public enum UICollectionViewSupplementaryKind: String{
+    case elementKindSectionFooter
+    case elementKindSectionHeader
+}
+
 public protocol Reuseable: AnyObject {
     static var reuseIdentifier: String { get }
 }
@@ -19,23 +24,24 @@ public extension Reuseable {
 
 public extension UICollectionView {
     func registerCell<T: UICollectionViewCell>(type: T.Type) where T: Reuseable {
-        self.register(UINib(nibName: T.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: T.reuseIdentifier)
+        self.register(UINib(nibName: T.reuseIdentifier, bundle: Bundle(for: T.self)), forCellWithReuseIdentifier: T.reuseIdentifier)
     }
     
     func dequeueReusableCell<T: UICollectionViewCell> (forIndexPath indexPath: IndexPath) -> T where T: Reuseable {
         return dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath as IndexPath) as! T
     }
+    
+    func registerSupplementaryView<T: UICollectionViewCell>(type: T.Type, kind: UICollectionViewSupplementaryKind) where T: Reuseable {
+        self.register(UINib(nibName: T.reuseIdentifier, bundle: Bundle(for: T.self)), forSupplementaryViewOfKind: kind.rawValue, withReuseIdentifier: T.reuseIdentifier)
+    }
 }
 
 public extension UITableView {
     func registerCell<T: UITableViewCell>(type: T.Type) where T: Reuseable {
-        self.register(UINib(nibName: T.reuseIdentifier, bundle: nil), forCellReuseIdentifier: T.reuseIdentifier)
+        self.register(UINib(nibName: T.reuseIdentifier, bundle: Bundle(for: T.self)), forCellReuseIdentifier: T.reuseIdentifier)
     }
     
     func dequeueReusableCell<T: UITableViewCell> (forIndexPath indexPath: IndexPath) -> T where T: Reuseable {
         return dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath as IndexPath) as! T
     }
 }
-
-
-
